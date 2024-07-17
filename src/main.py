e7a919f293ec7e29f80265484deb7dc226939357
Ali Beirami <alibeirami2003@gmail.com>
2024-07-17 10:04:41 +0200

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib as plt
+import time
+from sklearn.linear_model import LogisticRegression
+from utils import loadong_data, standardization
+
+
+
+
+
